299fef0511226fe4e5f9fce044cccea169cf39c6
coyote <megan.metelak@gmail.com>
2022-04-19 11:20:15 -0500

    first test

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,27 @@
+import { reducer } from '../utils/reducers';
+// import our actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+  } from '../utils/actions';
+  
+  // create a sample of what our global state will look like
+  const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+  };
+
+// test for updating the products list
+  test('UPDATE_PRODUCTS', () => {
+      // create a new state object
+      // reducer function accepts the current state object and action to update state with type and value
+    let newState = reducer(initialState, {
+      type: UPDATE_PRODUCTS,
+      products: [{}, {}]
+    });
+  
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+  });
